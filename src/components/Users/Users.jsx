aafcb810cddbb349e9d677e8853dee5334480d05
Egor <roso3600@gmail.com>
2023-06-21 00:12:32 +0300

    dddd

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,32 +1,34 @@
 import React from "react"
+import style from "./users.module.css"
 
 const Users = (props) => {
     return <div>
         {
-            props.users.map(u => <div key={u.userId} >
-                <span>
-                    <div>
-                        <img src="" alt="" />
-                    </div>
-                    <div>
-                        <button onClick={props.follow}> follow/unfollow </button>
-                    </div>
-                </span>
-                <span>
-                        <span>
-                            <div>{u.name}</div>
-                        <div>{u.statusMessage}</div>
-                    </span>
-            <span>
-                    <div>{u.location.state}</div>
-                    <div>{u.location.country}</div>
-                    </span>
-                </span>
-
+        props.users.map(u => <div className = {style.userBlock} key={u.userId} >
+        <span className={style.buttonImageBlock}>
+            <div className={style.userImage}>
+                <img src={u.photoUrl} alt="" />
             </div>
-            )
+            <div>
+                {u.followStatus}
+                
+            </div>
+        </span>
+        <span className={style.NameStatuseBlock}>
+            <span>
+                <div>{u.name}</div>
+                <div>{u.statusMessage}</div>
+            </span>
+            <span>
+                <div>{u.location.state}</div>
+                <div>{u.location.country}</div>
+            </span>
+        </span>
+
+    </div>
+    )
         }
-    </div> 
+</div>
 
 } 
 
